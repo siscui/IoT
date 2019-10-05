4b02a878dbed097432eb0eaae5eee4a93d8a173d
Mario Llanos <mario.llanos@appdirect.com>
2019-10-05 19:29:50 -0300

    Snapshot watch example.

--- read_db.py
+++ read_db.py
@@ -1,14 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from time import sleep
 
-cred = credentials.Certificate("siscui-firebase-adminsdk-qzw3h-981046b404.json")
+# Create a callback on_snapshot function to capture changes
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        print(u'Received document snapshot: {}'.format(doc.to_dict()))
+
+# Configure credentials and initalize database
+cred = credentials.Certificate('siscui-firebase-adminsdk-qzw3h-981046b404.json')
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-users_ref = db.collection(u'cultivos')
-docs = users_ref.stream()
+# Get document
+cultivos_ref = db.collection(u'cultivos')
+doc_ref = cultivos_ref.document(u'9gOhABtBYIToU5N4RRxV')
 
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+# Watch the document
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+input()

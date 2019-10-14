9c16bbd5e66ec7ea6a1db89eed680cd5f1210362
Mario Llanos <mario.llanos@appdirect.com>
2019-10-14 02:45:31 -0300

    Camera Controller.

---
+++ camera_controller.py
@@ -0,0 +1,18 @@
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+
+
+class CameraController:
+    def __init__(self):
+        self.camera = PiCamera()
+        self.camera.rotation = 180
+
+    def take_photo(self):
+        filename = f"./photos/{datetime.now().strftime('%Y%m%d%H%M%S')}.jpg"
+        with open(filename, 'wb') as file:
+            self.camera.start_preview()
+            sleep(0.5)
+            self.camera.capture(file)
+            self.camera.stop_preview()
+        return filename

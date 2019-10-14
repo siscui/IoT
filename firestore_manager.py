99243e8c0310be1cec5ff81b15954b8ef08d7554
Mario Llanos <mario.llanos@appdirect.com>
2019-10-13 23:33:15 -0300

    Firestore Manager.

--- firestore_manager.py
+++ firestore_manager.py
@@ -3,12 +3,16 @@ from firebase_admin import credentials, firestore
 
 
 class FirestoreManager:
-    def __init__(self, cred, doc_id):
+    def __init__(self, cred, conn):
         firebase_admin.initialize_app(credentials.Certificate(cred))
         self.doc_ref = firestore.client().collection(u'cultivos').document(doc_id)
 
     def set(self, data, merge=True):
         self.doc_ref.set(data, merge=merge)
 
+    def get(self):
+        return self.doc_ref.get().to_dict()
+
     def on_snapshot(self, handler):
         self.doc_ref.on_snapshot(handler)
+

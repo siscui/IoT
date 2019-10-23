e7d3461c302408b4645aae6e9ef71301044d55aa
Mario Llanos <mario.llanos@appdirect.com>
2019-10-22 22:36:09 -0300

    Firestore initialization fix.

--- firestore_manager.py
+++ firestore_manager.py
@@ -4,13 +4,13 @@ from firebase_admin import credentials, firestore
 
 class FirestoreManager:
     def __init__(self, cred, col_name):
+        firebase_admin.initialize_app(credentials.Certificate(cred))
         self.col_ref = firestore.client().collection(col_name)
         self.doc_ref = None
-        firebase_admin.initialize_app(credentials.Certificate(cred))
 
     def retrieve_doc(self, doc_id=None):
         self.doc_ref = self.col_ref.document(doc_id)
-        return self.doc_ref
+        return self.doc_ref.id
 
     def set(self, data, merge=True):
         self.doc_ref.set(data, merge=merge)

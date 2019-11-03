f155b9f5a22855e65c89428c52278ec467b427c0
Mario Llanos <mario.llanos@appdirect.com>
2019-11-02 21:04:25 -0300

    AI Models.

--- ai_model_manager.py
+++ ai_model_manager.py
@@ -7,14 +7,14 @@ class AIModelManager:
         self.image_path = image_path
 
     def detect_species(self):
-        return LabelImage(model_file="modelos/detect_specie_mobilenet_100_160.pb",
-                          label_file="modelos/detect_specie_mobilenet_100_160.txt",
+        return LabelImage(model_file="models/detect_species_mobilenet_100_160.pb",
+                          label_file="models/detect_species_mobilenet_100_160.txt",
                           filename=self.image_path).run()
 
     def detect_maturation(self, species):
         if species == 'tomate' or species == 'morron':
-            return LabelImage(model_file="modelos/maduro_tomate_100_160.pb",
-                              label_file="modelos/maduro_tomate_100_160.txt",
+            return LabelImage(model_file=f"models/maturity_{species}_100_160.pb",
+                              label_file=f"models/maturity_{species}_100_160.txt",
                               input_layer="Placeholder",
                               output_layer="final_result",
                               filename=self.image_path).run()
